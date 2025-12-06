9eac31a71e05f3a7ee477e6f15540218a6e907c8
smit patel <smit.dpatel9924@gmail.com>
2025-12-06 12:55:51 +0000

    feat: create user table migration

--- services/auth/auth/database/alembic/versions/empty.py
+++
@@ -1,7 +0,0 @@
-# This file is included temporarily to ensure the 'versions' directory 
-# is tracked in Git, as Alembic requires this folder to generate migration files.
-
-# ****************************** NOTE ******************************
-# Delete this file before creating your first migration.
-# If it remains, Alembic may raise an error during migration generation.
-# *******************************************************************
