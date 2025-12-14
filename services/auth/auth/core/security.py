797a5d9aa5335c9ea395c069983b9206e28060e9
smit patel <smit.dpatel9924@gmail.com>
2025-12-14 11:46:28 +0000

    refactor: replace Passlib with Bcrypt for password hashing

--- services/auth/auth/core/security.py
+++ services/auth/auth/core/security.py
@@ -4,8 +4,47 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    """
+    Validates a plaintext password against a bcrypt-hashed password.
+
+    Parameters
+    ----------
+    plain_password : str
+        The plaintext password to validate.
+
+    hashed_password : str
+        The bcrypt-hashed password to validate against.
+
+    Returns
+    -------
+    bool
+        True if the plaintext password matches the hashed password, otherwise False.
+    """
+    return checkpw(
+        bytes(plain_password, encoding="utf-8"),
+        bytes(hashed_password, encoding="utf-8"),
+    )
 
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    """
+    Create a bcrypt hash from the given plaintext password.
+
+    Parameters
+    ----------
+    password : str
+        The plaintext password to hash.
+
+    Returns
+    -------
+    str
+        The bcrypt-hashed password as a UTF-8 decoded string (includes salt).
+    """
+    password_hash_byte = hashpw(
+        bytes(password, encoding="utf-8"),
+        gensalt(),
+    )
+
+    password_hash_string = password_hash_byte.decode("utf-8")
+
+    return password_hash_string
