b166ff8a72e8e3ea05f3cb2f7619c32a7a44bd14
smit patel <smit.dpatel9924@gmail.com>
2026-05-16 10:44:01 +0000

    feat(auth): enhance JWT token validation to raise unauthorized exception for unauthenticated users

--- services/auth/auth/api/dependencies.py
+++ services/auth/auth/api/dependencies.py
@@ -8,6 +8,8 @@ from sqlmodel import Session
 from auth.core.config import settings
 from auth.core.security import authorize_user, decode_jwt_token
 from auth.database.db import engine
+from auth.exceptions.definitions.security_exceptions import UserUnauthorizedException
+from auth.schemas.auth_schemas import ParsedJWTPayload
 from auth.types.enums import TokenType, UserType
 
 
@@ -70,7 +72,7 @@ def RoleValidationDep(
     async def validate_jwt_token(
         token: TokenDep,
         session: SessionDep,
-    ) -> None:
+    ) -> ParsedJWTPayload:
         """
         Decode and validate a JWT access token and authorize the user by role.
 
@@ -92,9 +94,8 @@ def RoleValidationDep(
             None
         """
         if not roles:
-            # If no roles are provided, treat the route as public
-            # and skip role-based authorization checks
-            return
+            # If no roles are provided, treat user as unauthenticated
+            raise UserUnauthorizedException()
 
         token_payload = decode_jwt_token(token, expected_type= TokenType.ACCESS)
         authorize_user(
@@ -102,5 +103,6 @@ def RoleValidationDep(
             session=session,
             roles=roles,
         )
+        return token_payload
 
     return validate_jwt_token
