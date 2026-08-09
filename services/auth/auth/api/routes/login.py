97c8d20d3b74df5c19b3d9c9e650ce88f6eaa4a0
smit9924 <smit.dpatel9924@gmail.com>
2026-08-09 11:43:51 +0000

    chore: lint file

--- services/auth/auth/api/routes/login.py
+++ services/auth/auth/api/routes/login.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from auth.api.dependencies import SessionDep, decode_jwt_token
